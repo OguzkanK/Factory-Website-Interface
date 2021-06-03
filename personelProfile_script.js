52cf2f735182739646c0f2e50a4cfc324c556a90
Oguzkan-school <56897627+OguzkanK@users.noreply.github.com>
2021-06-03 14:28:40 +0300

    all items list added

--- personelProfile_script.js
+++ personelProfile_script.js
@@ -12,11 +12,15 @@ const listAllOrders_Btn = document.getElementById("list-allOrders");
 const backAllOrders_Btn = document.getElementById("back-from-allOrders");
 const listAllMachines_Btn = document.getElementById("list-allMachines");
 const backAllMachines_Btn = document.getElementById("back-from-allMachines");
+const listAllItems_Btn = document.getElementById("list-allItems");
+const backAllItems_Btn = document.getElementById("back-from-allItems");
 const allOrders_Area = document.getElementById("all-orders");
 const allMachines_Area = document.getElementById("all-machines");
+const allItems_Area = document.getElementById("all-items");
 
 let allOrdersHeight = 0;
 let allMachinesHeight = 0;
+let allItemsHeight = 0;
 
 const getAllOrders = function () {
   let xhttpGet = new XMLHttpRequest();
@@ -97,20 +101,20 @@ const getAllOperations = function () {
 };
 
 const fillMachineList = function (responseJson) {
-  let order = 0;
+  let varTime = 0;
   let templateRows = "75px";
   const allMachinesTemplateRows = [];
   for (const entry of responseJson) {
-    console.log(entry);
     const entryDiv = document.createElement("div");
     entryDiv.classList.add("grid-item");
     entryDiv.classList.add("entry-div");
+    const completeTime = document.createElement("div");
 
     for (let i = entry.length - 1; i > -1; i--) {
       if (i === entry.length - 1) {
-        const completeTime = document.createElement("div");
         completeTime.classList.add("grid-item");
-        completeTime.textContent = entry[i].completeTime;
+        varTime = entry[i].completeTime;
+        completeTime.textContent = varTime;
 
         entryDiv.appendChild(completeTime);
         entryDiv.appendChild(document.createElement("div"));
@@ -132,8 +136,10 @@ const fillMachineList = function (responseJson) {
           entryDiv.appendChild(wcName);
           entryDiv.appendChild(optName);
           entryDiv.appendChild(workTime);
-          order++;
           templateRows += ` 75px`;
+
+          if (entry[i].workTime === "-")
+            completeTime.textContent = varTime + ", Dolu makineler var";
         } else {
           const spAmount = document.createElement("div");
           spAmount.classList.add("grid-item");
@@ -146,12 +152,10 @@ const fillMachineList = function (responseJson) {
           entryDiv.appendChild(spName);
           entryDiv.appendChild(spAmount);
           entryDiv.appendChild(document.createElement("div"));
-          order++;
           templateRows += ` 75px`;
         }
       }
     }
-    console.log(entry);
     allMachinesTemplateRows.push(
       `${entry.length * 75 + (entry.length + 1) * 20 + 400}`
     );
@@ -165,64 +169,161 @@ const fillMachineList = function (responseJson) {
   allMachines_Area.style.gridTemplateRows = templateRows;
 };
 
+const getAllItems = function () {
+  let xhttpGet = new XMLHttpRequest();
+  xhttpGet.onreadystatechange = function () {
+    if (this.readyState === 4 && this.status === 200) {
+      let responseJson = JSON.parse(this.responseText);
+      fillItemList(responseJson);
+    }
+  };
+  xhttpGet.open("GET", "http://localhost:8080/GET/orders/allRelations", true);
+  xhttpGet.send();
+};
+
+const fillItemList = function (responseJson) {
+  let templateRows = "75px";
+  const allItemsTemplateRows = [];
+  for (const entry of responseJson) {
+    const entryDiv = document.createElement("div");
+    entryDiv.classList.add("grid-item");
+    entryDiv.classList.add("entry-div");
+    for (const [i, object] of entry.entries()) {
+      if (i === 0) {
+        const productName = document.createElement("div");
+        productName.classList.add("grid-item");
+        productName.textContent = object.productName;
+
+        const productType = document.createElement("div");
+        productType.classList.add("grid-item");
+        productType.textContent = object.productType;
+
+        const salable = document.createElement("div");
+        salable.classList.add("grid-item");
+        salable.textContent = object.salable;
+        entryDiv.appendChild(productName);
+        entryDiv.appendChild(productType);
+        entryDiv.appendChild(salable);
+      } else {
+        if (Object.keys(object).length % 2 === 1) {
+          const wcName = document.createElement("div");
+          wcName.classList.add("grid-item");
+          wcName.textContent = object.workcenterName;
+
+          const optName = document.createElement("div");
+          optName.classList.add("grid-item");
+          optName.textContent = object.operationName;
+
+          const workTime = document.createElement("div");
+          workTime.classList.add("grid-item");
+          workTime.textContent = object.workTime;
+
+          entryDiv.appendChild(wcName);
+          entryDiv.appendChild(optName);
+          entryDiv.appendChild(workTime);
+          templateRows += ` 75px`;
+        } else {
+          const spAmount = document.createElement("div");
+          spAmount.classList.add("grid-item");
+          spAmount.textContent = object.subProductAmount;
+
+          const spName = document.createElement("div");
+          spName.classList.add("grid-item");
+          spName.textContent = object.subProductName;
+
+          entryDiv.appendChild(spName);
+          entryDiv.appendChild(spAmount);
+          entryDiv.appendChild(document.createElement("div"));
+          templateRows += ` 75px`;
+        }
+      }
+    }
+    allItemsTemplateRows.push(
+      `${entry.length * 75 + (entry.length + 1) * 20 + 400}`
+    );
+    entryDiv.style.gridTemplateRows = templateRows;
+    templateRows = " 75px";
+    allItems_Area.appendChild(entryDiv);
+    allItemsHeight += entry.length * 75 + (entry.length + 1) * 20 + 390;
+  }
+  templateRows = "";
+  for (const rows of allItemsTemplateRows) templateRows += `${rows}px `;
+  allItems_Area.style.gridTemplateRows = templateRows;
+};
+
+getAllItems();
 getAllOperations();
 getAllOrders();
 
-listAllOrders_Btn.addEventListener("click", function () {
-  listAllOrders_Btn.classList.toggle("hidden");
-  backAllOrders_Btn.classList.toggle("hidden");
+const backToProfile = function (targetArea, targetBtn, targetBack) {
+  targetBtn.classList.toggle("hidden");
+  targetBack.classList.toggle("hidden");
 
-  hideResultList();
-
-  allOrders_Area.classList.toggle("grid-hide");
-  allOrders_Area.style.height = `${allOrdersHeight}px`;
-  allOrders_Area.style.margin = `auto`;
-  allOrders_Area.style.marginTop = `5vh`;
-});
-
-backAllOrders_Btn.addEventListener("click", function () {
-  listAllOrders_Btn.classList.toggle("hidden");
-  backAllOrders_Btn.classList.toggle("hidden");
-
-  showResultList();
-
-  allOrders_Area.classList.toggle("grid-hide");
-  allOrders_Area.style.height = `0px`;
-  allOrders_Area.style.margin = `0`;
-});
-
-listAllMachines_Btn.addEventListener("click", function () {
-  listAllMachines_Btn.classList.toggle("hidden");
-  backAllMachines_Btn.classList.toggle("hidden");
-
-  hideResultList();
-
-  allMachines_Area.classList.toggle("grid-hide");
-  allMachines_Area.style.height = `${allMachinesHeight}px`;
-  allMachines_Area.style.margin = `auto`;
-  allMachines_Area.style.marginTop = `5vh`;
-});
-
-backAllMachines_Btn.addEventListener("click", function () {
-  listAllMachines_Btn.classList.toggle("hidden");
-  backAllMachines_Btn.classList.toggle("hidden");
-
-  showResultList();
-
-  allMachines_Area.classList.toggle("grid-hide");
-  allMachines_Area.style.height = `$0px`;
-  allMachines_Area.style.margin = `0`;
-});
-
-const showResultList = function () {
   resultList.classList.toggle("grid-hide");
   resultList.style.backgroundColor = "#a35709";
-  resultList.style.height = "40vh";
+  resultList.style.height = "55vh";
   resultList.style.margin = "25vh auto";
+
+  targetArea.classList.toggle("grid-hide");
+  targetArea.style.height = `0px`;
+  targetArea.style.margin = `0`;
 };
-const hideResultList = function () {
+
+const LeaveProfile = function (
+  targetArea,
+  targetBtn,
+  targetBack,
+  targetHeight
+) {
+  targetBtn.classList.toggle("hidden");
+  targetBack.classList.toggle("hidden");
+
   resultList.classList.toggle("grid-hide");
   resultList.style.backgroundColor = "transparent";
   resultList.style.height = 0;
   resultList.style.margin = 0;
+
+  targetArea.classList.toggle("grid-hide");
+  targetArea.style.height = `${targetHeight}px`;
+  targetArea.style.margin = `auto`;
+  targetArea.style.marginTop = `5vh`;
 };
+
+listAllOrders_Btn.addEventListener("click", function () {
+  LeaveProfile(
+    allOrders_Area,
+    listAllOrders_Btn,
+    backAllOrders_Btn,
+    allOrdersHeight
+  );
+});
+
+listAllMachines_Btn.addEventListener("click", function () {
+  LeaveProfile(
+    allMachines_Area,
+    listAllMachines_Btn,
+    backAllMachines_Btn,
+    allMachinesHeight
+  );
+});
+
+listAllItems_Btn.addEventListener("click", function () {
+  LeaveProfile(
+    allItems_Area,
+    listAllItems_Btn,
+    backAllItems_Btn,
+    allItemsHeight
+  );
+});
+
+backAllOrders_Btn.addEventListener("click", function () {
+  backToProfile(allOrders_Area, listAllOrders_Btn, backAllOrders_Btn);
+});
+
+backAllMachines_Btn.addEventListener("click", function () {
+  backToProfile(allMachines_Area, listAllMachines_Btn, backAllMachines_Btn);
+});
+
+backAllItems_Btn.addEventListener("click", function () {
+  backToProfile(allItems_Area, listAllItems_Btn, backAllItems_Btn);
+});
